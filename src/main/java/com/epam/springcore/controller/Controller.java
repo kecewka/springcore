514c129a59f103059d4fb83b78e1109010260ea1
Askar Aytkazin <a_aytkazin@kbtu.kz>
2024-01-15 14:32:28 +0600

    Added some kind of Facade? TODO: -Improve Testing, add repository tests (didn't understand)

--- src/main/java/com/epam/springcore/controller/Controller.java
+++ src/main/java/com/epam/springcore/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.epam.springcore.controller;
 
+import com.epam.springcore.entity.Trainee;
+import com.epam.springcore.entity.User;
 import com.epam.springcore.service.TraineeService;
 import com.epam.springcore.service.TrainerService;
 import com.epam.springcore.service.TrainingService;
@@ -7,8 +9,10 @@ import com.epam.springcore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping(value = "/api")
+import java.time.LocalDate;
+
+//@RestController
+//@RequestMapping(value = "/api")
 public class Controller {
 
     private final UserService userService;
@@ -22,6 +26,13 @@ public class Controller {
         this.traineeService = traineeService;
         this.trainerService = trainerService;
         this.trainingService = trainingService;
+    }
+
+    public Trainee createTrainee(String firstname, String lastname, LocalDate dateOfBirth, String address) {
+        User user = new User(1L, firstname, lastname, "", "", true);
+        Trainee trainee = new Trainee(1L, dateOfBirth, address, user, null, null);
+        traineeService.createTrainee(trainee);
+        return trainee;
     }
 
 //    @GetMapping
